bfc1c0be7e9213c76bf3079ae9111f59ee7bd2e8
yuyi1266173 <yuyi1266173@163.com>
2018-04-26 21:06:28 +0800

    add pagelist

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import models
 
 # Create your views here.
 
 def test(request):
 	#return render(request, 'index.html', {'hello': 'Hello Blog...'})
 	return render(request, 'blog/index.html')
+
+def pagelist(request):
+	articles = models.Article.objects.all()
+	return render(request, 'blog/pagelist.html', {'articles': articles})
