2191441e49a0692ab5abe08e33b4af8bdbb50405
yuyi1266173 <yuyi1266173@163.com>
2018-04-26 20:44:16 +0800

    add Article models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+	title = models.CharField(max_length = 32, default = 'TITLE')
+	content = models.TextField(null = True)   #允许为空
+
+	def __str__(self):
+		return self.title
+
+
