ceb1dadce0fb8dd59cdfe300ed8bcb93b8024a9a
yuyi1266173 <yuyi1266173@163.com>
2018-04-26 23:28:24 +0800

    add ArticleAdmin

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 
-admin.site.register(models.Article)
+class ArticleAdmin(admin.ModelAdmin):
+	list_display = ('title', 'content')
+
+admin.site.register(models.Article, ArticleAdmin)
